9868c07a51a34d9c1050d22e019ef3b0c19778be
RiccardoStella <riccardostella004@gmail.com>
2021-10-09 16:21:57 +0200

    ver. 1.2.3

--- game.js
+++ game.js
@@ -120,7 +120,7 @@ getNewQuestion = () => {
     }
 
     questionCounter++
-    progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
+    progressText.innerText = `Domanda ${questionCounter} / ${MAX_QUESTIONS}`
     progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS) * 100}%`
     
     const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
